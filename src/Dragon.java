81c9e2f8055adae46f89f4d234ccad7b6612f1ed
91091791 <91091791@mvusd.net>
2021-08-20 08:41:53 -0700

    Edits to the Dragon class: constructors and getters/setters

--- src/Dragon.java
+++ src/Dragon.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class Dragon {
-}
+public class Dragon extends Enemies
+{
+    //Instance Variables
+    private int wingLength;
+    private int numWings;
+
+    //Constructors
+    public Dragon (boolean hasClaws, String color, int wingLength, int numWings)
+    {
+        super(hasClaws, color);
+        this.wingLength = wingLength;
+        this.numWings = numWings;
+    }
+    public Dragon(boolean hasClaws, String color, int wingLength)
+    {
+        super(hasClaws, color);
+        this.wingLength = wingLength;
+        this.numWings = 0;
+    }
+
+    //Getters and Setters
+    public int getWingLength()
+    {
+        return wingLength;
+    }
+    public void setWingLength(int wingLength)
+    {
+        this.wingLength = wingLength;
+    }
+    public int getNumWings()
+    {
+        return numWings;
+    }
+    public void setNumWings(int numWings)
+    {
+        this.numWings = numWings;
+    }
+
+    //Brain Methods
+
+    //toString
+    public String toString()
+    {
+        String output = "\nThis dragon has " + numWings + " wings. Wing length (feet): " + wingLength;
+        output += "\n" + super.toString();
+        return output;
+    }
+}//end Dragon class
--- src/Golem.java
+++ src/Golem.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class Golem {
-}
+public class Golem extends Enemies
+{
+    //Instance Variables
+    private String elementType;
+    private int height;
+
+    //Constructors
+    public Golem (boolean hasClaws, String color, String elementType, int height)
+    {
+        super(hasClaws, color);
+        this.elementType = elementType;
+        this.height = height;
+    }
+    public Golem (boolean hasClaws, String color)
+    {
+        super(hasClaws, color);
+        this.elementType = "none";
+        this.height = 0;
+    }
+
+    //Getters and Setters
+    public String getElementType()
+    {
+        return elementType;
+    }
+    public void setElementType(String elementType)
+    {
+        this.elementType = elementType;
+    }
+    public int getHeight()
+    {
+        return height;
+    }
+    public void setHeight(int height)
+    {
+        this.height = height;
+    }
+    //Brain Methods
+
+    //toString
+    public String toString()
+    {
+
+        String output = "\nThis Golem is a " + elementType + " type and is " +
+                height + " meters tall.";
+        output += "\n" + super.toString();
+        return output;
+    }
+}//end Golem class
